8e2b7d0072d9803af93fcc5daa497397d279eaae
Henning <leutz@pcsg.de>
2026-07-27 13:28:10 +0200

    test: enable shipping shims in CI
    Mark the shimmed Shipping package as installed during PHPUnit bootstrap.
    
    CI omits this optional package.
    
    The package flag lets shipping-path tests exercise their test doubles.
    
    Related: quiqqer/payment-paypal#35

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -10,3 +10,11 @@ if (!defined('QUIQQER_AJAX')) {
 
 require_once __DIR__ . '/../../../../bootstrap.php';
 require_once __DIR__ . '/package-autoload.php';
+
+// CI does not install optional packages. Mark Shipping as available so its test shims
+// exercise the same PayPal code paths locally and in CI.
+$PackageManager = QUI::getPackageManager();
+$Installed = new ReflectionProperty($PackageManager, 'installed');
+$installed = $Installed->getValue($PackageManager);
+$installed['quiqqer/shipping'] = true;
+$Installed->setValue($PackageManager, $installed);
